96cd693c98a02a03ffb7bc33dcaaec607b6f9e02
IceDynamix <aerozocker@gmail.com>
2020-01-03 01:04:53 +0100

    Remove unnecessary directives

--- ConsoleGameCollection/Program.cs
+++ ConsoleGameCollection/Program.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleGameCollection
-{
-	class Program
+namespace ConsoleGameCollection {
+    class Program
 	{
 		static void Main(string[] args)
 		{
